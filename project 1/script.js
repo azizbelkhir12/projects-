6924699192e4eada495fc1807e4c230a92495add
azizbelkhir12 <mouhamedazizbelkhir20@gmail.com>
2024-09-28 20:30:00 +0100

    Add files via upload
    project1

---
+++ project 1/script.js
@@ -0,0 +1,40 @@
+const inputBox = document.getElementById("input-box");
+const listeContainer = document.getElementById("liste-container");
+
+function addTask() {
+    if(inputBox.value === '') {
+        alert("You must write something");
+    }
+    else {
+        let li = document.createElement("li") ;
+        li.innerHTML = inputBox.value ;
+        listeContainer.appendChild(li) ;
+        let span = document.createElement("span") ;
+        span.innerHTML = "\u00d7" ;
+        li.appendChild(span) ;
+    }
+    inputBox.value = "" ;
+	saveData() ;
+}
+
+listeContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+		saveData() ;
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+		saveData() ;
+    }
+}, false);
+
+function saveData() {
+	localStorage.setItem("data", listeContainer.innerHTML);
+}
+
+function showTask(){
+	listeContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
+
